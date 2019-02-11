686b02a52bac583877509518e1b14dcd7325bf50
KuddusImam <46973562+KuddusImam@users.noreply.github.com>
2019-02-11 17:10:44 -0500

    Update UserStory_2_Test.java

--- src/test/java/com/cybertek/tests/UserStory_2_Test.java
+++ src/test/java/com/cybertek/tests/UserStory_2_Test.java
@@ -16,63 +16,90 @@ public class UserStory_2_Test extends TestBase {
 
 
     public UserStory_2 userStory_2;
-
-
-    @Test //1,2,3
-    public void test1() {
+    @BeforeMethod
+    public void cc(){
         userStory_2 = new UserStory_2();
         wait.until(ExpectedConditions.visibilityOf(userStory_2.createBtn));
+        //wait.until(ExpectedConditions.visibilityOf(userStory_2.createBtn));
         userStory_2.createBtn.click();
-
+    }
+    @Test //1,2,3
+    public void test1() {
         BrowserUtils.wait(2);
         List<WebElement> mylist = driver.findElements(By.xpath("//input[@class='o_input ui-autocomplete-input' and @autocomplete='off']"));
         mylist.get(0).click();
-
         userStory_2.createAndEdit.click();
         wait.until(ExpectedConditions.visibilityOf(userStory_2.radioButton));
-
         //2
         Assert.assertTrue(userStory_2.radioButton.isSelected());
-
         //3
         userStory_2.discard.click();
-
     }
-
     @Test //4
     public void test4() {
-        userStory_2 = new UserStory_2();
-        wait.until(ExpectedConditions.visibilityOf(userStory_2.createBtn));
-        userStory_2.createBtn.click();
-
         BrowserUtils.wait(2);
         List<WebElement> mylist = driver.findElements(By.xpath("//input[@class='o_input ui-autocomplete-input' and @autocomplete='off']"));
         mylist.get(0).sendKeys("John Smith");
-
         mylist.get(1).sendKeys("123 fairfax rd. farirfax VA");
-        //BrowserUtils.wait(100);
-
+        //BrowserUtils.wait(5);
         new Actions(driver).pause(2000).perform();
+        wait.until(ExpectedConditions.visibilityOf( userStory_2.CancelButton));
         userStory_2.CancelButton.click();
         userStory_2.discard2.click();
-
-
-
-
-
-
-
+    }
+    @Test
+    public void test5(){
+        BrowserUtils.wait(2);
+        List<WebElement> mylist = driver.findElements(By.xpath("//input[@class='o_input ui-autocomplete-input' and @autocomplete='off']"));
+        mylist.get(0).click();
+        userStory_2.searchMore.click();
+        //BrowserUtils.wait(5);
+    }
+    @Test
+    public void test6(){
+        BrowserUtils.wait(2);
+        List<WebElement> mylist = driver.findElements(By.xpath("//input[@class='o_input ui-autocomplete-input' and @autocomplete='off']"));
+        mylist.get(0).click();
+        userStory_2.createAndEdit.click();
+        userStory_2.internalNotes.click();
+        //BrowserUtils.wait(5);
+    }
+    @Test
+    public void test7(){
+        BrowserUtils.wait(2);
+        List<WebElement> mylist = driver.findElements(By.xpath("//input[@class='o_input ui-autocomplete-input' and @autocomplete='off']"));
+        mylist.get(0).click();
+        userStory_2.createAndEdit.click();
+        userStory_2.salesPurchases.click();
+        //BrowserUtils.wait(5);
+    }
+    @Test
+    public void test8(){
+        BrowserUtils.wait(2);
+        List<WebElement> mylist = driver.findElements(By.xpath("//input[@class='o_input ui-autocomplete-input' and @autocomplete='off']"));
+        mylist.get(0).click();
+        userStory_2.createAndEdit.click();
+        userStory_2.invoice.click();
+    }
+    @Test
+    public void test9(){
+        BrowserUtils.wait(2);
+        List<WebElement> mylist = driver.findElements(By.xpath("//input[@class='o_input ui-autocomplete-input' and @autocomplete='off']"));
+        mylist.get(0).sendKeys("John Smith");
+        mylist.get(1).sendKeys("123 fairfax rd. farirfax VA");
+        new Actions(driver).pause(2000).perform();
+        wait.until(ExpectedConditions.visibilityOf(userStory_2.Create));
+        //userStory_2.save1.click();
+        userStory_2.Create.click();
+    }
+    @Test
+    public void test10(){
+        BrowserUtils.wait(2);
+        List<WebElement> mylist = driver.findElements(By.xpath("//input[@class='o_input ui-autocomplete-input' and @autocomplete='off']"));
+        mylist.get(0).click();
+        userStory_2.createAndEdit.click();
+        userStory_2.addItem.click();
     }
 
 
-
-
-
-
-
-
-
-
-
-
-    }
+}
